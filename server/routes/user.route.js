4112919802e3801ae8ec7d33e37a45b6176c085b
ddld93 <umar.jere@gmail.com>
2022-06-07 06:13:17 +0100

    commit

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,7 +3,6 @@ const uuid = require('uuid').v4;
 const userCtrl = require("../controller/user.controller")
 const User = require("../model/user.model")
 const jwt = require("jsonwebtoken");
-const config = require('../../config');
 
 
 
@@ -112,7 +111,7 @@ module.exports = (express, UPLOADS) => {
         name: user.fullName,
         userType: user.userType,
         company: user.company
-      }, config.keys.jwt, { expiresIn: '1h' })
+      }, "+Y9FYqpJxJGeRy9aj1NOCbmAPZt/IKqPuDBJNf+gbuuK7nXuC82UA1kKSQju+TiqxhQwYCJgPcBn0lIdkA4KDj9F++U14AeVeCn3sbxBxqsykd7UOXEMrwUN808Io1cr02V5n3jm9Z6vVGxxbfkjepQ63zF2M6U7IkTNW15wGnM6cST6uPHVZOL1tl0bcosh536JCdIE6VNsaWgFfNSEbKCncDeQ9GQlUwDgrgQbeNQRyFYVIAeJx2F5Fv69e5/oZk25hRZPUMrXfrxGiWdmUX71df39OCycsD4aNog4xz3o9bjT6tJIqqAX7mQK5Gjce5VpilqY+z0SZVeylc5E6Q==", { expiresIn: '1h' })
       res.json({ status: "success", user: user, token });
     } catch (error) {
       res.send(error);
