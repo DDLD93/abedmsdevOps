0e882b75df453bca47277395e04450eee174b6e3
ddld93 <umar.jere@gmail.com>
2022-07-21 07:31:40 +0100

    commit

--- server/connection/mongodb.connection.js
+++ server/connection/mongodb.connection.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 var host = process.env.MONGODB_HOST || "database"
 var port = process.env.MONGODB_PORT || 27017
-var connString = "mongodb+srv://abedmis:16001105@cluster0.fljiocn.mongodb.net/?retryWrites=true&w=majority"
 const url = `mongodb://${host}:${port}/system`
 module.exports = () => {
-    let options = { 
+    let options = {
         useNewUrlParser: true,
-    }
-
+        // useFindAndModify: false,
+        // useCreateIndex: true,
+        // poolSize: 10,
+        keepAlive: true,
+        connectTimeoutMS: 30000,
+        useUnifiedTopology: true
+    };
+    
     const db = mongoose.connection;
     db.on('connected', () => {
         console.log('We are connected to mongodb');
@@ -15,10 +20,10 @@ module.exports = () => {
     db.on('error', (err) => {
         console.log('Error connecting to mongodb ', err);
     });
-
+    
     db.on('disconnect', () => {
         console.log('Oops we are disconnected from mongodb');
-        mongoose.connect(connString, options);
+        mongoose.connect(url,options);
     });
-    mongoose.connect(connString, options);
+    mongoose.connect(url,options);
 }
