a66574b9bc274f8fce53f59a84e82b7993f531a4
ddld93 <umar.jere@gmail.com>
2022-07-25 11:18:27 +0100

    commit

--- server/connection/mongodb.connection.js
+++ server/connection/mongodb.connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 var host = process.env.MONGODB_HOST || "database"
 var port = process.env.MONGODB_PORT || 27017
-const url = `mongodb://${host}:${port}/system`
+const url = `mongodb://ddld:ddld@${host}:${port}/system`
+
 module.exports = () => {
     let options = {
         useNewUrlParser: true,
